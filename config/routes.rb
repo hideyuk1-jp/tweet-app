94e0aa27c5924463712ed4373ecafe1c47c72b8d
hideyuk1-jp <hh11028+github@gmail.com>
2019-04-22 21:18:31 +0900

    投稿詳細画面を作成、投稿時刻をJSTで表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'home#top'
   get '/posts/index' => 'posts#index'
+  get '/posts/:id' => 'posts#show'
 end
