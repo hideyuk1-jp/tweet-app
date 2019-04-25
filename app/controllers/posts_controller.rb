5d01bce2a27d9cfd954877e5971bcbddc1fa48ba
hideyuk1-jp <hh11028+github@gmail.com>
2019-04-25 22:35:26 +0900

    新規投稿・投稿編集・投稿削除の機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,43 @@
 class PostsController < ApplicationController
   def index
     if params[:keyword]
-      @posts = Post.where("content LIKE ?", "%#{params[:keyword]}%")
+      @posts = Post.where("content LIKE ?", "%#{params[:keyword]}%").order(created_at: :desc)
       @keyword = params[:keyword]
     else
-      @posts = Post.all
+      @posts = Post.all.order(created_at: :desc)
     end
   end
 
   def show
     @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    flash[:notice] = "投稿を送信しました。"
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    flash[:notice] = "投稿を編集しました。"
+    redirect_to("/posts/#{params[:id]}")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました。"
+    redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get '/posts/index' => 'posts#index'
   post '/posts/search' => 'posts#index'
+  get '/posts/new' => 'posts#new'
+  post '/posts/create' => 'posts#create'
   get '/posts/:id' => 'posts#show'
+  get '/posts/:id/edit' => 'posts#edit'
+  post '/posts/:id/update' => 'posts#update'
+  get '/posts/:id/destroy' => 'posts#destroy'
 end
