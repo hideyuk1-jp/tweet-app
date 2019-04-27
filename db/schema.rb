b26696ac7b482301ec4f08bd59efeb6fa55cefc8
hideyuk1-jp <hh11028+github@gmail.com>
2019-04-27 16:03:44 +0900

    postsにuser_idを追加して適当に値を代入

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_213007) do
+ActiveRecord::Schema.define(version: 2019_04_27_065003) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
