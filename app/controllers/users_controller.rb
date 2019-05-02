890c12010b4080d898037c397367342ac5045058
hideyuk1-jp <hh11028+github@gmail.com>
2019-05-02 21:21:37 +0900

    アソシエーションを使って、いいねした投稿一覧を取得するように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
 
   def likes
     @user = User.find_by(id: params[:id])
-    @likes = Like.where(user_id: @user.id).order(created_at: :desc)
+    @posts = @user.like_posts.page(params[:page]).per(POST_PER)
   end
 
   def edit
