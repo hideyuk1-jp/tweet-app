9485fcad4339381914fdc6883a8024df9c228468
hideyuk1-jp <hh11028+github@gmail.com>
2019-05-06 10:47:36 +0900

    conntentsモデルのマイグレーション、cmmentsコントローラーの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_175459) do
+ActiveRecord::Schema.define(version: 2019_05_06_014426) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
