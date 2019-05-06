050bc3c02d8e50893ef533ef61a416530f08bc4b
hideyuk1-jp <hh11028+github@gmail.com>
2019-05-06 10:52:27 +0900

    アソシエーション、バリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :liked_users, -> { order('created_at DESC') }, through: :likes, source: :user
+  has_many :comments, dependent: :destroy
+  has_many :commented_users, -> { order('created_at DESC') }, through: :comments, source: :user
 
   validates :content, {presence: true, length: { maximum: 140 }}
   validates :user_id, {presence: true}
