a7561bdc6518e4fd91cd6dae8eafe5e6c1a8a692
hideyuk1-jp <hh11028+github@gmail.com>
2019-05-06 12:34:30 +0900

    バリデーションを訂正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :user_id, :post_id, presence: true
-  validates :user_id, uniqueness: { scope: :post_id }
   validates :content, {presence: true, length: { maximum: 140 }}
 end
